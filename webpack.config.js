694ca34fbf8f8f16e63cdd7288c043c11cb58eee
Ariel Biernacki <arielbiernacki@gmail.com>
2019-11-25 21:27:23 +0100

    Fix reloading bug and add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -12,6 +12,15 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true
+        }
+      },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
@@ -26,22 +35,22 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
+            loader: MiniCssExtractPlugin.loader
           },
           'css-loader',
           'postcss-loader'
-        ],
+        ]
       },
       {
         test: /\.html$/i,
-        use: ['html-loader'],
+        use: ['html-loader']
       },
       {
         test: /\.(jpe?g|png)$/,
         use: [
           {
-          loader: 'file-loader',
-          options: {
+            loader: 'file-loader',
+            options: {
               name: '[name].[ext]',
               outputPath: 'img/',
               publicPath: 'img/'
@@ -58,6 +67,6 @@ module.exports = {
     new MiniCssExtractPlugin()
   ],
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-  },
-};
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  }
+}
