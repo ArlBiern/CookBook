ad45eb7d658968371a71f6e05b7c29a8b6bf5bd8
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2019-11-28 00:17:55 +0100

    Add layout without RWD & slider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './css/main.css'
+import './css/curosity.css'
+import './css/cards.css'
+import './css/selected.css'
+import './css/list.css'
+import './css/error.css'
+import './css/dropdown.css'
+import './css/glide.core.min.css'
+import './css/glide.theme.min.css'
+import Glide from '@glidejs/glide'
 
-const loadScripts = () => {
-  console.log('Scripts start')
-}
-
-document.addEventListener('DOMContentLoaded', loadScripts)
+new Glide('.glide', { autoplay: 4000 }).mount()
--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,10 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../'
+              }
           },
           'css-loader',
           'postcss-loader'
